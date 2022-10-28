7105ee3a6912f44f82d8f62fd893aa3d56b4e572
Mauricio Generoso <mauricio.marquesgeneroso@sky.uk>
2022-10-28 13:54:10 +0100

    include algorithms

--- src/main/java/uk/sky/kata/algorithms/BinarySearching.java
+++ src/main/java/uk/sky/kata/algorithms/BinarySearching.java
@@ -1,9 +1,46 @@
 package uk.sky.kata.algorithms;
 
-public class BinarySearching implements IntArraySearching{
+public class BinarySearching implements IntArraySearching {
+
+//    @Override
+//    public int search(int[] input, int target) {
+//        var startIndex = 0;
+//        var endIndex = input.length - 1;
+//
+//        while (startIndex <= endIndex) {
+//            var middle = (startIndex + endIndex) / 2;
+//
+//            if (input[middle] == target) {
+//                return middle;
+//            }
+//
+//            if (target > input[middle]) {
+//                startIndex = middle + 1;
+//            } else {
+//                endIndex = middle - 1;
+//            }
+//        }
+//
+//        return -1;
+//    }
 
     @Override
     public int search(int[] input, int target) {
+        return binarySearch(input, 0, input.length - 1, target);
+    }
+
+    public static int binarySearch(int[] array, int startIndex, int endIndex, int target) {
+        if (startIndex <= endIndex) {
+            var middle = (startIndex + endIndex) / 2;
+
+            if (array[middle] == target) {
+                return middle;
+            } else if (array[middle] > target) {
+                return binarySearch(array, startIndex, middle - 1, target);
+            }
+
+            return binarySearch(array, middle + 1, endIndex, target);
+        }
 
         return -1;
     }
--- src/main/java/uk/sky/kata/algorithms/LinearSearching.java
+++ src/main/java/uk/sky/kata/algorithms/LinearSearching.java
@@ -5,6 +5,13 @@ public class LinearSearching implements IntArraySearching{
     @Override
     public int search(int[] input, int target) {
 
+        var length = input.length;
+        for (var i = 0; i < length; i++) {
+            if (input[i] == target) {
+                return i;
+            }
+        }
+
         return -1;
     }
 }
